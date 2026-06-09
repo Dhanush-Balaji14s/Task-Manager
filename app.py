39502c8e9c7f3c6b58d3c7379806ad883b9b39e1
Dhanush-Balaji14s <dhanushbalaji14s@gmail.com>
2026-06-09 13:17:51 +0530

    Task Manager project file is added

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f'<Task {self.id}>'
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def index():
+    tasks = Task.query.all()
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add_task():
+    task_content = request.form['content']
+
+    if task_content:
+        new_task = Task(content=task_content)
+        db.session.add(new_task)
+        db.session.commit()
+
+    return redirect('/')
+
+@app.route('/complete/<int:id>')
+def complete_task(id):
+    task = Task.query.get_or_404(id)
+    task.completed = not task.completed
+    db.session.commit()
+    return redirect('/')
+
+@app.route('/delete/<int:id>')
+def delete_task(id):
+    task = Task.query.get_or_404(id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
